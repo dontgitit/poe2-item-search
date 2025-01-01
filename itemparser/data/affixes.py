85c35dfb7d081563a3e539c1ec89a6cdd363b7d8
Ilya Brin <dontgitit@users.noreply.github.com>
2025-01-01 14:43:18 -0600

    add more affixes

--- itemparser/data/affixes.py
+++ itemparser/data/affixes.py
@@ -28,8 +28,11 @@ def r(mod):
     return fr"([+-]?\d+)%? (?:to )?{mod}( \(implicit\))?"
 
 affix_re = [
+    # prefixes
     (r('maximum Life'), 'stat_3299347043'),
     (r('maximum Mana'), 'stat_1050105434'),
+    (r('increased Spell Damage'), 'stat_2974417149'),
+    # suffixes
     (r('Strength'), 'stat_4080418644'),
     (r('Dexterity'), 'stat_3261801346'),
     (r('Intelligence'), 'stat_328541901'),
@@ -39,5 +42,15 @@ affix_re = [
     (r('Chaos Resistance'), 'stat_2923486259'),
     (r('all Elemental Resistances'), 'stat_2901986750'),
     (r('increased Mana Regeneration Rate'), 'stat_789117908'),
+    (r('increased Critical Hit Chance'), 'stat_587431675'),
+    (r('increased Critical Damage Bonus'), 'stat_3556824919'),
+    (r('increased Cast Speed'), 'stat_2891184298'),
+    # Either
+    (r('increased Rarity of Items found'), 'stat_3917489142'),
+    # TODO - use correct accuracy filter depending on item class. for now just do non-weapon...
+    # other accuracy
+    (r('Accuracy Rating'), 'stat_803737631'),
+    # weapon accuracy
+    (r('Accuracy Rating'), 'stat_691932474'),
 ]
 
